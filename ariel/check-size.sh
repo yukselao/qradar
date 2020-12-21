6b5b9293fe2fcac657e1f978d0c3e6dac44db1c1
Ali Okan Yüksel <aokany@gmail.com>
2020-12-22 00:12:51 +0300

    Update check-size.sh

--- ariel/check-size.sh
+++ ariel/check-size.sh
@@ -8,5 +8,7 @@ for d in $@; do
 du -sh payloads/$d
 du -sh records/$d
 done
-
+for d in $@; do
+        echo $(du -sh -c $(find records/$d -type d -name lucene -o -name super) |grep total|awk '{print $1}')   records/lucene+super
+done
 
