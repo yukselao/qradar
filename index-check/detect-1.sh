a0263277f84e0b0edc55f73abdadf512c1e135a5
Ali Okan Yüksel <aokany@gmail.com>
2021-11-22 22:42:27 +0300

    Update detect-1.sh

--- index-check/detect-1.sh
+++ index-check/detect-1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 year=$1
-
+outputfile=/tmp/detect-1.out-$(hostname)
+>$outputfile
 function printusage(){
         echo 'Usage:
 '$0' 2021 SourceIP
@@ -22,13 +23,16 @@ for d in $(find /store/ariel/events/records/$year -type d -mindepth 3 -maxdepth 
         du -sh $d/super/${index}~0 &>/dev/null
         if [ $? -ne 0 ]; then
                 echo "$d/super/${index}~0;0"
+                echo "$d/super/${index}~0;0" >> $outputfile
         else
                 c=$(ls -1 $d/super/${index}~* 2>/dev/null |wc -l)
                 if [[ "$c" -gt "1" ]]; then
                         if [[ "$debug" == "true" ]] && [[ "$c" -gt "1" ]]; then
                                 echo "$d/super/${index}~0;$c;$(ls -1 $d/super/${index}~* |tr '\n' ',')"
+                                echo "$d/super/${index}~0;$c;$(ls -1 $d/super/${index}~* |tr '\n' ',')" >> $outputfile
                         else
                                 echo "$d/super/${index}~0;$c"
+                                echo "$d/super/${index}~0;$c" >> $outputfile
                         fi
                 fi
         fi
