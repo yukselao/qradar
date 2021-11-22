54c497c8c4c9a122e7b90115e91f83b81fcc4c29
Ali Okan Yüksel <aokany@gmail.com>
2021-11-22 21:11:30 +0300

    Update detect-1.sh

--- index-check/detect-1.sh
+++ index-check/detect-1.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-for d in $(find events/records -type d -mindepth 4 -maxdepth 4 2>/dev/null); do
-        du -sh $d/super/SourceIP~0 &>/dev/null
+year=$1
+
+function printusage(){
+        echo 'Usage:
+'$0' 2021 SourceIP
+
+'
+
+}
+[[ -z "$year" ]] && echo "Year parameter not found" && printusage && exit 1
+
+index=$2
+
+export i=0
+for param in $@; do
+        export i="$(expr $i + 1)"
+        [[ "$param" == "-d" ]] && debug="true"
+done
+for d in $(find /store/ariel/events/records/$year -type d -mindepth 3 -maxdepth 3 2>/dev/null); do
+        du -sh $d/super/${index}~0 &>/dev/null
         if [ $? -ne 0 ]; then
-                echo "$d/super/SourceIP~0 doesn't exists"
+                echo "$d/super/${index}~0;0"
         else
-                c=$(ls -1 $d/super/SourceIP~* 2>/dev/null |wc -l)
+                c=$(ls -1 $d/super/${index}~* 2>/dev/null |wc -l)
                 if [[ "$c" -gt "1" ]]; then
-                        echo "$d/super/SourceIP~0 duplucate file | $(ls -1 $d/super/SourceIP~* 2>/dev/null |tr '\n' ', ')"
+                        if [[ "$debug" == "true" ]] && [[ "$c" -gt "1" ]]; then
+                                echo "$d/super/${index}~0;$c;$(ls -1 $d/super/${index}~* |tr '\n' ',')"
+                        else
+                                echo "$d/super/${index}~0;$c"
+                        fi
                 fi
         fi
 done
