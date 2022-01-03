416a5c466aa42b5f994d6186a91eb0c78e15f4bb
Ali Okan Yüksel <aokany@gmail.com>
2022-01-03 12:49:49 +0300

    Create qactl.sh_test2.sh

---
+++ qactl.sh_test2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "INFO indexleri yeniden olustur, search yap, data file counter=0, index file counter>0 kontrolu gerceklestir. Eger index file counter 0'dan buyukse, ariel index'leri dogru sekilde kullaniliyor demektir."
+./qactl -db "2021/12/31 11:00" 60
+for searchid in $(./qactl -listallsearches |grep 551955 | cut -d';' -f2 |cut -d'=' -f2); do ./qactl -deletesearch $searchid; done;
+./qactl -checkdatafilecount 551955 "2021-12-31 10:00:00" "2021-12-31 10:59:00" 0.5.0.1
